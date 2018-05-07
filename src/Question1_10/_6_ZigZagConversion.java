52bb2c0cb656ff5dba6d68525aea62806eb8d7e7
xychencs <xychencs@163.com>
2018-05-07 17:18:29 +0800

    leetCode 第6题 Z型字符串变化

---
+++ src/Question1_10/_6_ZigZagConversion.java
@@ -0,0 +1,33 @@
+package Question1_10;
+
+public class _6_ZigZagConversion {
+	public String convert(String s, int numRows) {
+        StringBuilder sb = new StringBuilder();
+		char c[] = s.toCharArray();
+		int len;
+		if (numRows == 1) {
+			len = 1;
+		} else {
+			len = 2 * (numRows - 1);
+		}
+		for (int i = 0; i < numRows; i++) {
+			int j = i;
+			if (j == 0 || j == numRows - 1) {
+				while (j < c.length) {
+					sb.append(c[j]);
+					j = j + len;
+				}
+			} else {
+				int L = 2 * (numRows - 1 - j);
+				while (j < c.length) {
+					sb.append(c[j]);
+					j += L;
+					L = len - L;
+				}
+			}
+
+		}
+
+		return sb.toString();
+    }
+}
